3b3f69bf360bbf22ff505f513944f0527107cda6
Joseph Francis <hookedupjoe@users.noreply.github.com>
2023-09-06 10:42:42 -0400

    initial commit

---
+++ app/pages/Home/index.js
@@ -0,0 +1,130 @@
+(function (ActionAppCore, $) {
+
+    var SiteMod = ActionAppCore.module("site");
+
+    //~thisPageSpecs//~
+var thisPageSpecs = {
+	"pageName": "Home",
+	"pageTitle": "Home",
+	"navOptions": {
+		"topLink": true,
+		"sideLink": true
+	}
+}
+//~thisPageSpecs~//~
+
+    var pageBaseURL = 'app/pages/' + thisPageSpecs.pageName + '/';
+
+    //~layoutOptions//~
+thisPageSpecs.layoutOptions = {
+        baseURL: pageBaseURL,
+        north: false,
+        east: false,
+        west: false,
+        center: { html: "center" },
+        south: false
+    }
+//~layoutOptions~//~
+
+    //~layoutConfig//~
+thisPageSpecs.layoutConfig = {
+        west__size: "500"
+        , east__size: "250"
+    }
+//~layoutConfig~//~
+    //~required//~
+thisPageSpecs.required = {
+
+    }
+//~required~//~
+
+    var ThisPage = new SiteMod.SitePage(thisPageSpecs);
+
+    var actions = ThisPage.pageActions;
+
+    ThisPage._onPreInit = function (theApp) {
+        //~_onPreInit//~
+
+//~_onPreInit~//~
+    }
+
+    ThisPage._onInit = function () {
+        //~_onInit//~
+
+//~_onInit~//~
+    }
+
+
+    ThisPage._onFirstActivate = function (theApp) {
+        //~_onFirstActivate//~
+
+//~_onFirstActivate~//~
+        ThisPage.initOnFirstLoad().then(
+            function () {
+                //~_onFirstLoad//~
+// ThisApp.util.clearToTop(ThisPage.getSpot$('countdown').get(0));
+// ThisApp.delay(100).then(function(){
+//   $('html').css('background-color','#008E97');
+// })
+
+var countDownDate = new Date('Sep 10 2023 16:25:00 EDT');
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  var tmpOut = days + ' <span class="time"> DAYS</span><br/> ' + hours + ' <span class="time"> HOURS</span><br/> ' + minutes + ' <span class="time"> MINUTES</span><br/> ' + seconds + ' <span class="time"> SECONDS</span><br/>';
+  
+  ThisPage.loadSpot('countdown', tmpOut);
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+//~_onFirstLoad~//~
+                ThisPage._onActivate();
+            }
+        );
+    }
+
+
+    ThisPage._onActivate = function () {
+        //~_onActivate//~
+
+//~_onActivate~//~
+    }
+
+    ThisPage._onResizeLayout = function (thePane, theElement, theState, theOptions, theName) {
+        //~_onResizeLayout//~
+
+//~_onResizeLayout~//~
+    }
+
+    //------- --------  --------  --------  --------  --------  --------  -------- 
+    //~YourPageCode//~
+function dateWithTimeZone(timeZone, year, month, day, hour, minute, second){
+  let date = new Date(Date.UTC(year, month, day, hour, minute, second));
+
+  let utcDate = new Date(date.toLocaleString('en-US', { timeZone: "UTC" }));
+  let tzDate = new Date(date.toLocaleString('en-US', { timeZone: timeZone }));
+  let offset = utcDate.getTime() - tzDate.getTime();
+
+  date.setTime( date.getTime() + offset );
+
+  return date;
+};
+//~YourPageCode~//~
+
+})(ActionAppCore, $);
